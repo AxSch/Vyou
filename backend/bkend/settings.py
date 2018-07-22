aca7376150345b468a6c92fbaaf9791229dd6c3c
K.A. Schuneman <alex_schuneman@outlook.com>
2018-07-22 13:33:39 +0100

    feat: Added template path

--- backend/bkend/settings.py
+++ backend/bkend/settings.py
@@ -61,7 +61,9 @@ REST_FRAMEWORK = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
