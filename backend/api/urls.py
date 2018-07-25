ebadada78043b8bb956fdcb1ca0a5655563d0857
K.A. Schuneman <alex_schuneman@outlook.com>
2018-07-26 00:12:58 +0100

    feat\; Added routing & API endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from api import views
 from rest_framework_swagger.views import get_swagger_view
+from rest_framework import routers
 
 schema_view = get_swagger_view(title='test API')
+router = routers.DefaultRouter()
+router.register('users', views.UserView)
+# router.register('auth', include('django.contrib.auth.urls')),
 
 urlpatterns = [
     path('', schema_view),
+    path('', include(router.urls)),
 ]
