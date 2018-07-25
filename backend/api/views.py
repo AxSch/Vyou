41c59efae50a11bd07ab68a2c039201da5469d99
K.A. Schuneman <alex_schuneman@outlook.com>
2018-07-26 00:12:05 +0100

    feat: Implemented rest_framework viewset for Users

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
+from .serializer import UserModelSerializer
+from .models import User
 
 # Create your views here.
 def home(request):
   return render(request, '../templates/base.html')
+
+# @api_view(["GET"])
+# def get_user(request, pk):
+#   user = User.objects.get(id=pk)
+#   serializer = UserModelSerializer(user)
+#   return Response(serializer.data)
+
+class UserView(viewsets.ModelViewSet):
+  queryset = User.objects.all()
+  serializer_class = UserModelSerializer
+
