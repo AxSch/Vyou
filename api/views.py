3be83a517c82577a0eec6f02e24a13bb6c9ff47c
K.A. Schuneman <alex_schuneman@outlook.com>
2018-08-29 00:47:37 +0100

    feat: Added custom user model, serializer & implemented url & view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import User
+from .serializer import UserModelSerializer
 # Create your views here.
+
+class UserView(viewsets.ModelViewSet):
+  queryset = User.objects.all()
+  serializer_class = UserModelSerializer
