1b15a36d01f5637d1f15f202363d1573ce1a4081
K.A. Schuneman <alex_schuneman@outlook.com>
2018-08-29 01:06:58 +0100

    chore: Edited wsgi to use production settings

--- vapp/wsgi.py
+++ vapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vapp.production_settings")
 
 application = get_wsgi_application()
