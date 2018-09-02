ead0898e4e3d902e52814cd2f2e4ea316571e10a
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-02 21:33:48 +0100

    feat: Added site id for eb and also RDS config

--- vapp/production_settings.py
+++ vapp/production_settings.py
@@ -1,5 +1,7 @@
 from .settings import *
 
+SITE_ID = 'vyou-dev.eu-west-2.elasticbeanstalk.com'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
 ]
@@ -10,3 +12,26 @@ WEBPACK_LOADER = {
             'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.prod.json'),
         }
 }
+
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'vyoudevdb',
+            'USER': 'Kozzimodo',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
