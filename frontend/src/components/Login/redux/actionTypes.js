8f7816cae1854cae9ad5e7648fd479b332a0de75
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-11 00:03:28 +0100

    feat: Updated actions & added actionTypes

---
+++ frontend/src/components/Login/redux/actionTypes.js
@@ -0,0 +1,7 @@
+const actionTypes = {
+  REQUEST_LOGIN: 'REQUEST_LOGIN',
+  LOGIN_SUCCESS: 'LOGIN_SUCCESS',
+  LOGIN_FAILURE: 'LOGIN_FAILURE',
+}
+
+export default actionTypes;
--- frontend/src/components/Login/redux/actions.js
+++ frontend/src/components/Login/redux/actions.js
@@ -1,19 +1,19 @@
-import { loginActTypes } from './actionTypes';
+import { actionTypes } from './actionTypes';
 
 
 const loginRequest = (email,password) => ({
-  type: loginActTypes.REQUEST_LOGIN,
+  type: actionTypes.REQUEST_LOGIN,
 });
 
 const loginSuccess = (email,password,) => ({
-  type: loginActTypes.LOGIN_SUCCESS,
+  type: actionTypes.LOGIN_SUCCESS,
   payload: {
     email
   },
 });
 
 const loginFailure = (email, error) => ({
-  type: loginActTypes.LOGIN_FAILURE,
+  type: actionTypes.LOGIN_FAILURE,
   payload: {
     email,
     error
