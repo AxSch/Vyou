f5da094aa082f4c65763407bb52faddc2e39b3cf
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-10 23:09:33 +0100

    feat: Added provider & connected the redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import NotFound from './components/ErrorPages/NotFound';
-import './App.css';
+import appStore from './store/store';
 import Login from './components/Login/Login';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/accounts/login" component={Login} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/accounts/login" component={Login} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
