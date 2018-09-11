a574e95d4eaa7e218c231c4ae4bef7eb6fe3e6d8
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-12 00:38:48 +0100

    feat: Updated store to use developer tools

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,17 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import {appReducer, storeState} from '../mainRedux/mainReducer';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const loggerMiddleware = createLogger();
+const enhancer = composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware));
 
 const appStore = createStore(
       appReducer,
       storeState,
-      applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-      )
+      enhancer
   );
 
 export default appStore;
