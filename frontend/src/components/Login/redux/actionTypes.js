1a7a231404619043a9fb059378253961786e9bba
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-11 21:34:48 +0100

    feat: Added action type for loggedOut action & updated login

--- frontend/src/components/Login/redux/actionTypes.js
+++ frontend/src/components/Login/redux/actionTypes.js
@@ -1,7 +1,8 @@
 const actionTypes = {
-  REQUEST_LOGIN: 'REQUEST_LOGIN',
+  LOGIN_REQUEST: 'LOGIN_REQUEST',
   LOGIN_SUCCESS: 'LOGIN_SUCCESS',
   LOGIN_FAILURE: 'LOGIN_FAILURE',
+  LOGGED_OUT: 'LOGGED_OUT',
 }
 
 export default actionTypes;
