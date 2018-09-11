93a16e0ed530d28d370d0aa0fae0e1b87a256c0f
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-11 21:42:07 +0100

    feat: Added cases for all login/logout actions  & relevant state changes

--- frontend/src/components/Login/redux/reducer.js
+++ frontend/src/components/Login/redux/reducer.js
@@ -1,12 +1,37 @@
+import actionTypes from "./actionTypes";
+
 const initialState = {
   userID: '',
-  userName: '',
+  userEmail: '',
+  loggingIn:  false,
+  loggedIn: false,
+  loggedOut: true,
+  error: null,
 }
 
 const loginReducer = (state=initialState, action) => {
   switch(action.type){
+      case actionTypes.LOGIN_REQUEST:
+        return {
+          loggingIn: true,
+        }
+      case actionTypes.LOGIN_SUCCESS:
+        return {
+          loggedIn: true,
+          loggedOut: false,
+          userID: action.userID,
+          userEmail: action.email, 
+        }
+      case actionTypes.LOGIN_FAILURE:
+        return {
+          error: action.error,
+        }
+      case actionTypes.LOGGED_OUT:
+        return {
+          loggedOut: true,
+        }
     default:
-    return state;
+      return state;
   }
 }
 
