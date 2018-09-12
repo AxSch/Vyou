3de981286d15297874a191986e57cb2ffd284a76
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-12 23:10:03 +0100

    feat: Created a basic container for login component

--- frontend/src/components/Login/LoginContainer.js
+++ frontend/src/components/Login/LoginContainer.js
@@ -1,14 +1,21 @@
 import { connect } from 'react-redux';
-import Login from './Login';
-import loginDispatcher from './redux/dispatchers';
+import LoginPage from './Login';
+import { bindActionCreators } from 'redux';
+import {loginDispatcher, logoutDispatcher} from './redux/dispatchers';
 
 const mapStateToProps = (state, ownProps) => ({
     userID: state.login,
 });
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-    login: loginDispatcher(dispatch),
-});
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return { 
+        loginAction: (email, password) => {
+            dispatch(loginDispatcher(email, password))
+        }
+    }
+} 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+const LoginContainer = connect(mapStateToProps, mapDispatchToProps)(LoginPage);
+
+export default LoginContainer;
 
