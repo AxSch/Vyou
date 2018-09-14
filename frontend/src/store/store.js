c9beb59b6a23046bb74bf5f36e2cea767af5b7d3
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-14 22:12:13 +0100

    feat: Added persistStore to the store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import appReducer from '../mainRedux/mainReducer';
+import { persistStore, persistReducer } from 'redux-persist';
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const loggerMiddleware = createLogger();
@@ -11,5 +13,7 @@ const appStore = createStore(
       appReducer,
       enhancer
   );
+
+persistStore(appStore);
 
 export default appStore;
