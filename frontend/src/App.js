a538ce368dfcfb829ea428076a02aabae34ab54a
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-15 16:46:35 +0100

    feat: Added persistGate & state persistence to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,25 @@ import React, { Component } from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import NotFound from './components/ErrorPages/NotFound';
-import appStore from './store/store';
+import {appStore, persistedStore} from './store/store';
 import LoginContainer from './components/Login/LoginContainer';
 import ProfileContainer from './components/AccountPage/Profile/ProfileContainer';
+import { PersistGate } from 'redux-persist/integration/react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Provider store={appStore}>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/accounts/login" component={LoginContainer} />
-            <Route exact path="/accounts/profile/" component={ProfileContainer} />
-            <Route component={NotFound} />
-          </Switch>
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistedStore}>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/accounts/login" component={LoginContainer} />
+              <Route exact path="/accounts/profile" component={ProfileContainer} />
+              <Route component={NotFound} />
+            </Switch>
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     );
   }
