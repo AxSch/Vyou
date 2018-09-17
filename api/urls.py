467235ce2fce79616906c262f005078793bba58c
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-17 22:19:45 +0100

    chore: Commented out auth

--- api/urls.py
+++ api/urls.py
@@ -5,13 +5,13 @@ from rest_framework_swagger.views import get_swagger_view
 from rest_framework import routers
 # from .views import UserView
 
-schema_view = get_swagger_view(title='test API')
+schema_view = get_swagger_view(title='Vyou API')
 router = routers.DefaultRouter()
 # router.register('users', UserView)
 
 urlpatterns = [
     path('', schema_view),
     path('', include(router.urls)),
-    path('auth/', include('rest_auth.urls')),
+    # path('auth/', include('knox.urls')),
     # path('auth/signup/', include('rest_auth.registration.urls')),
 ]
