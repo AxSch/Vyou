26e927b9e57d29d5a29777c1b9182e7d98fd782c
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-18 00:05:39 +0100

    feat: Updated link to the correct url used by django allauth

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -14,7 +14,7 @@ class HomePage extends Component {
         <div>
           <h1>NavBar</h1>
           <ul>
-            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/accounts/login">Login</Link></li>
             <li>Logout</li>
             <li>About Us</li>
           </ul>
