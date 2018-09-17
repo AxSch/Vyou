164af087e17ab45f61fb64233722f4b6af28b587
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-18 00:05:15 +0100

    feat: Re-added Login & Profile routes & containers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import NotFound from './components/ErrorPages/NotFound';
+import LoginContainer from './components/Login/LoginContainer';
+import ProfileContainer from './components/AccountPage/Profile/ProfileContainer';
 import {appStore, persistedStore} from './store/store';
 import HomePage from './components/HomePage/HomePage';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -15,7 +17,8 @@ class App extends Component {
           <BrowserRouter>
             <Switch>
               <Route exact path="/" component={HomePage} />
-              {/* <Route exact path="/accounts/profile/" component={ProfileContainer} /> */}
+              <Route exact path="/accounts/login/" component={LoginContainer} />
+              <Route exact path="/accounts/profile/" component={ProfileContainer} />
               <Route component={NotFound} />
             </Switch>
           </BrowserRouter>
