d201d2868dafc1810dc17a86da5e7a35c9b33732
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-19 00:09:44 +0100

    fix: Resolved missing comma

--- vapp/settings.py
+++ vapp/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'vyou.eu-west-2.elasticbeanstalk.com'
+    'vyou.eu-west-2.elasticbeanstalk.com',
     'vyou-dev.eu-west-2.elasticbeanstalk.com',
     'vyou-staging.eu-west-2.elasticbeanstalk.com',
 ]
