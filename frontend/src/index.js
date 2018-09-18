042257c9915908e4f2585c9e55273a6308eb77b0
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-18 23:40:37 +0100

    refact: Added store, provider, router & persist gate

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import {appStore, persistedStore} from './store/store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={appStore}>
+    <PersistGate loading={null} persistor={persistedStore}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+document.getElementById('root'));
 registerServiceWorker();
