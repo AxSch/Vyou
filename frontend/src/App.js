ecb3c190a2c22ceec568ef59fb46de2aa493ad9c
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-18 23:38:18 +0100

    feat: Implemented routes to be rendered by this component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,12 @@
-import React, { Component } from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import NotFound from './components/ErrorPages/NotFound';
-import LoginContainer from './components/Login/LoginContainer';
-import ProfileContainer from './components/AccountPage/Profile/ProfileContainer';
-import {appStore, persistedStore} from './store/store';
-import HomePage from './components/HomePage/HomePage';
-import { PersistGate } from 'redux-persist/integration/react';
+import { Component } from 'react';
+import { routes } from './components/Routes/Routes';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={appStore}>
-        <PersistGate loading={null} persistor={persistedStore}>
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route exact path="/accounts/login/" component={LoginContainer} />
-              <Route exact path="/accounts/profile/" component={ProfileContainer} />
-              <Route component={NotFound} />
-            </Switch>
-          </BrowserRouter>
-        </PersistGate>
-      </Provider>
-    );
+          routes()
+        );
   }
 }
 
