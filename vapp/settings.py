df5c6a7959f9561a192f9854961b1581c8384d48
K.A. Schuneman <alex_schuneman@outlook.com>
2018-09-19 00:05:16 +0100

    feat: Added url for staging to the allowed hosts

--- vapp/settings.py
+++ vapp/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'vyou-dev.eu-west-2.elasticbeanstalk.com'
+    'vyou-dev.eu-west-2.elasticbeanstalk.com',
+    'vyou-staging.eu-west-2.elasticbeanstalk.com',
 ]
 
 
