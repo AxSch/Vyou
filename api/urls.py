58dc9856459c080f0c56920ec1ffc5c1067552e0
K.A. Schuneman <alex_schuneman@outlook.com>
2018-10-13 18:36:37 +0100

    refact & feat: Replaced old code with new urls & schema

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework.authtoken import views as auth_view
+from django.urls import include, path
 from rest_framework_swagger.views import get_swagger_view
-from rest_framework import routers
-# from .views import UserView
 
-schema_view = get_swagger_view(title='Vyou API')
-router = routers.DefaultRouter()
-# router.register('users', UserView)
+schema_view = get_swagger_view(title='Vyou Api')
 
 urlpatterns = [
-    path('', schema_view),
-    path('', include(router.urls)),
-    path('auth/', include('rest_auth.urls')),
-    # path('auth/signup/', include('rest_auth.registration.urls')),
+  path('', schema_view),
+  path('accounts/', include('users_api.urls'))
 ]
