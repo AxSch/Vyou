ac66816cab725c1e22af56be440a53f95cd03b6a
K.A. Schuneman <alex_schuneman@outlook.com>
2018-10-30 22:16:32 +0000

    feat: Added new endpoints for registration, auth & token

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
+from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Vyou Api')
 
 urlpatterns = [
   path('', schema_view),
-  path('', include('users_api.urls'))
+  path('', include('users_api.urls')),
+  path('auth/', include('rest_auth.urls')),
+  path('register/', include('rest_auth.registration.urls')),
+  path('refresh-token/', refresh_jwt_token),
 ]
