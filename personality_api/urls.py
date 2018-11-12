97d3ec9bf4392c90a320f3cbb3c7e521919c26da
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-12 18:35:20 +0000

    refact: Changed the paths for questions

--- personality_api/urls.py
+++ personality_api/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from .views import PersonalityApi, EnergyFlowApi, EnergyLevelApi, EnergyMappingApi
 
 urlpatterns = [
-  path('personality questions/', PersonalityApi.as_view()),
-  path('energy flow questions/', EnergyFlowApi.as_view()),
-  path('energy level questions/', EnergyLevelApi.as_view()),
-  path('energy mapping questions/', EnergyMappingApi.as_view()),
+  path('personality/', PersonalityApi.as_view()),
+  path('energy_flow/', EnergyFlowApi.as_view()),
+  path('energy_level/', EnergyLevelApi.as_view()),
+  path('energy_mapping/', EnergyMappingApi.as_view()),
 ]
