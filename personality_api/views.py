3d7b597a82bc39dea6ac0a1e4edea22dc330b79d
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-12 14:31:46 +0000

    feat: Added view for EnergyFlow Questions

--- personality_api/views.py
+++ personality_api/views.py
@@ -10,3 +10,9 @@ class QuestionsApi(APIView):
     with open('api_data/Questions.json', 'r') as file:
       questions_data = json.load(file)
       return Response(questions_data)
+
+class EnergyFlowApi(APIView):
+  def get(self, request, format=None):
+    with open('api_data/EnergyFlowQuestions.json', 'r') as file:
+      energy_flow_data = json.load(file)
+      return Response(energy_flow_data)
