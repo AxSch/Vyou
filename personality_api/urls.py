f1bb679c6eff61625ba3e061a6448333916c78b3
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-12 15:31:09 +0000

    feat: Added EnergyLevel view & added associated endpoints

--- personality_api/urls.py
+++ personality_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
-from .views import QuestionsApi, EnergyFlowApi
+from .views import PersonalityQuestionsApi, EnergyFlowApi, EnergyLevelApi
 
 urlpatterns = [
-  path('personality questions/', QuestionsApi.as_view()),
-  path('energy flow questions/', EnergyFlowApi.as_view())
+  path('personality questions/', PersonalityQuestionsApi.as_view()),
+  path('energy flow questions/', EnergyFlowApi.as_view()),
+  path('energy level questions/', EnergyLevelApi.as_view()),
 ]
--- personality_api/views.py
+++ personality_api/views.py
@@ -5,13 +5,20 @@ from rest_framework import authentication
 import json
 
 # Create your views here.
-class QuestionsApi(APIView):
+class PersonalityQuestionsApi(APIView):
   def get(self, request, format=None):
     with open('api_data/Questions.json', 'r') as file:
       questions_data = json.load(file)
       return Response(questions_data)
 
 class EnergyFlowApi(APIView):
+  def get(self, request, format=None):
+    with open('api_data/EnergyFlowQuestions.json', 'r') as file:
+      energy_flow_data = json.load(file)
+      return Response(energy_flow_data)
+
+
+class EnergyLevelApi(APIView):
   def get(self, request, format=None):
     with open('api_data/EnergyFlowQuestions.json', 'r') as file:
       energy_flow_data = json.load(file)
