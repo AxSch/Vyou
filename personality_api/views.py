1b8f6c181efec968efed6b916dda0b22e1dde3f1
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-12 13:37:46 +0000

    feat: Added QuestionsApi view

--- personality_api/views.py
+++ personality_api/views.py
@@ -1,1 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication
+import json
+
+# Create your views here.
+class QuestionsApi(APIView):
+  def get(self, request, format=None):
+    with open('api_data/Questions.json', 'r') as file:
+      questions_data = json.load(file)
+      return Response(questions_data)
