26c4b6f01041ffe14d08020df664ab20d6af46c5
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-13 14:16:53 +0000

    feat: Added basic view

--- question_ans/views.py
+++ question_ans/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+
+from .models import PersonalityAnswers
+from .serializers import PersonalityAnswersSerializer
 
 # Create your views here.
+class PersonalityAnswersView(viewsets.ModelViewSet):
+  queryset = PersonalityAnswers.objects.all()
+  serializer_class = PersonalityAnswersSerializer
