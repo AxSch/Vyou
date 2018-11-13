9fd9fe094988ce306c9fca65ec02d8fefd09e64b
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-13 14:36:32 +0000

    refact: Altered the endpoint for answers

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
   path('', include('users_api.urls')),
   path('', include('user_profile.urls')),
   path('questions/', include('personality_api.urls')),
-  path('', include('question_ans.urls')),
+  path('answers/', include('question_ans.urls')),
   path('auth/', include('rest_auth.urls')),
   path('registration/', include('rest_auth.registration.urls')),
   path('refresh-token/', refresh_jwt_token),
