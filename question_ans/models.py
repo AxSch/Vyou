bbd3bd0006e2d6477bf89c41161b6af4dfda43ef
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-13 13:58:14 +0000

    feat: Added basic model

--- question_ans/models.py
+++ question_ans/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class PersonalityAnswers(models.Model):
+  questionId = models.IntegerField()
+  question = models.CharField(max_length=64)
+  category_name = models.CharField(max_length=32)
+  scale_alpha = models.IntegerField()
+  answer_text = models.CharField(max_length=16)
+  answer_score = models.IntegerField()
+
+
+  def __str__(self):
+    return self.answer_score
