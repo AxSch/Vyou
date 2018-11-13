a5cea9554bc69284e7f0b5a76510f48c8791d10e
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-13 14:30:23 +0000

    feat: Added Energy related views

--- question_ans/views.py
+++ question_ans/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import viewsets
 
-from .models import PersonalityAnswers
-from .serializers import PersonalityAnswersSerializer
+from .models import PersonalityAnswers, EnergyLevelAnswers, EnergyFlowAnswers, EnergyMappingAnswers
+from .serializers import PersonalityAnswersSerializer, EnergyLevelAnswersSerializer, EnergyFlowAnswersSerializer, EnergyMappingAnswersSerializer
 
 # Create your views here.
 class PersonalityAnswersView(viewsets.ModelViewSet):
   queryset = PersonalityAnswers.objects.all()
   serializer_class = PersonalityAnswersSerializer
+
+
+class EnergyLevelAnswersView(viewsets.ModelViewSet):
+  queryset = EnergyLevelAnswers.objects.all()
+  serializer_class = EnergyLevelAnswersSerializer
+
+class EnergyFlowAnswersView(viewsets.ModelViewSet):
+  queryset = EnergyFlowAnswers.objects.all()
+  serializer_class = EnergyFlowAnswersSerializer
+
+class EnergyMappingAnswersView(viewsets.ModelViewSet):
+  queryset = EnergyMappingAnswers.objects.all()
+  serializer_class = EnergyMappingAnswersSerializer
+
