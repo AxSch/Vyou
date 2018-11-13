70a7ccef1922d390b2967ea7d04041d181808f11
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-13 17:36:55 +0000

    feat: Added questionsReducer to main reducer

--- frontend/src/mainRedux/mainReducer.js
+++ frontend/src/mainRedux/mainReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { loginReducer } from '../components/Login/redux/reducer';
+import { questionsReducer } from '../components/QuestionsPage/redux/reducer';
 
 const appReducer = combineReducers({
-  login: loginReducer
+  login: loginReducer,
+  questions: questionsReducer,
 })
 
 export default appReducer;
