4ed7bf74de7d72bd739ba0ecc12fb1b4191f8b23
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-15 16:36:08 +0000

    feat: Added question method to render each question

--- frontend/src/components/QuestionsPage/Personality/PersonalityQuestions.js
+++ frontend/src/components/QuestionsPage/Personality/PersonalityQuestions.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 
 class PersonalityQuestions extends Component {
-
+    Question(questions) {
+        return questions.map((question) => {
+            // console.log(question);
+                return (
+                    <Fragment>
+                        <li key={question.id}>
+                            {question.fields.question}
+                        </li>
+                    </Fragment>
+                );
+        });
+    }
     render() {
         const { questions } = this.props;
         return (
             <div>
                 <h2>Personality Questions</h2>
+                <ul>
+                    {this.Question(questions)}
+                </ul>
             </div>
         );
     }
