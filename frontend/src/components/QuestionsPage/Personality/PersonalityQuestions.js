f258d789959f0efd650bf3e70593108ba8f26e3d
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-15 16:55:29 +0000

    feat: Added radio buttons to Question -- might make component

--- frontend/src/components/QuestionsPage/Personality/PersonalityQuestions.js
+++ frontend/src/components/QuestionsPage/Personality/PersonalityQuestions.js
@@ -5,11 +5,24 @@ import PropTypes from 'prop-types';
 class PersonalityQuestions extends Component {
     Question(questions) {
         return questions.map((question) => {
-            // console.log(question);
                 return (
                     <Fragment>
                         <li key={question.id}>
                             {question.fields.question}
+                            <div>
+                                <label htmlFor="stronglyAgree">Strongly Agree</label>
+                                <input type="radio" name="answers" value={3} id="stronglyAgree" /> 
+                                <label htmlFor="stronglyAgree">Slightly Agree</label>
+                                <input type="radio" name="answers" value={2} id="stronglyAgree" />  
+                                <label htmlFor="stronglyAgree">Agree</label>
+                                <input type="radio" name="answers" value={1} id="stronglyAgree" />  
+                                <label htmlFor="stronglyAgree">Disagree</label>
+                                <input type="radio" name="answers" value={-1} id="stronglyAgree" />  
+                                <label htmlFor="stronglyAgree">Slightly Disagree</label>
+                                <input type="radio" name="answers" value={-2} id="stronglyAgree" />  
+                                <label htmlFor="stronglyAgree">Strongly Disagree</label>
+                                <input type="radio" name="answers" value={-3} id="stronglyAgree" />                         
+                            </div>
                         </li>
                     </Fragment>
                 );
