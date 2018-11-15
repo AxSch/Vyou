fe5dc1702aa18c7d526858f875286da0813b04fc
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-15 21:41:00 +0000

    feat: Implemented the QuestionFactory component

--- frontend/src/components/QuestionsPage/Personality/PersonalityQuestions.js
+++ frontend/src/components/QuestionsPage/Personality/PersonalityQuestions.js
@@ -1,31 +1,15 @@
 import React, { Component, Fragment } from 'react';
-import Answers from '../../Answers/Answers';
 import PropTypes from 'prop-types';
+import QuestionFactory from '../Question/QuestionFactory';
 
 
 class PersonalityQuestions extends Component {
-    Question(questions) {
-            return questions.filter((question) => question.fields.QuestionCategory.id === 1).map((question) => {
-                return (
-                    <Fragment>
-                        <li key={question.id}>
-                            {question.fields.question}
-                            {question.fields.QuestionCategory.id}
-                            {question.fields.QuestionCategory.category_name}
-                            <Answers />
-                        </li>
-                    </Fragment>
-                );
-        })
-    }
     render() {
         const { questions } = this.props;
         return (
             <div>
                 <h2>Personality Questions</h2>
-                <ul>
-                    {this.Question(questions)}
-                </ul>
+                <QuestionFactory questionsArray={questions} index={1} />
             </div>
         );
     }
