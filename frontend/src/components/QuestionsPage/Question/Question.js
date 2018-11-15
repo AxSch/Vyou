420bf152611c29bdd47bdb1e54ae2af909bd2bef
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-15 21:38:43 +0000

    feat: Built basic Questions component

---
+++ frontend/src/components/QuestionsPage/Question/Question.js
@@ -0,0 +1,25 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import Answers from '../../Answers/Answers';
+
+
+class Question extends Component {
+    render() {
+        const { question } = this.props;
+        // console.log(question.fields);
+        return (
+          <Fragment>
+              <li key={question.id}>
+                {question.fields.question}
+                <Answers />
+            </li>
+          </Fragment>
+        );
+    }
+}
+
+Question.PropTypes = {
+    question: PropTypes.object
+}
+
+export default Question;
