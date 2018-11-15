12dc6a05434dcb868fdb70072c2d95f53dcffefa
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-15 21:39:16 +0000

    feat: Added a factory to handle questions and their Categories(--wip)

---
+++ frontend/src/components/QuestionsPage/Question/QuestionFactory.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Question from './Question';
+
+const renderQuestion = (questions, category_id) => {
+  return questions.filter((question) => question.fields.QuestionCategory.id === category_id).map((question) => {
+      return (
+          <Question question={question}/>
+      );
+  })
+}
+
+const renderQuestionCategory = (questions, index) => {
+  const questionList = questions.filter((question) => question.fields.QuestionCategory.id === index);
+  return (
+    <h3>{questionList[0].fields.QuestionCategory.category_name}</h3>
+  );
+}
+
+class QuestionFactory extends Component {
+  render() {
+    const { questionsArray, index } = this.props;
+    return (
+      <div>
+        {renderQuestionCategory(questionsArray, index)}
+      <ul>
+       {renderQuestion(questionsArray, index)}
+      </ul>
+    </div>
+    );
+  }
+}
+
+QuestionFactory.PropTypes = {
+  questionsArray: PropTypes.array,
+  index: PropTypes.number
+}
+
+export default QuestionFactory;
