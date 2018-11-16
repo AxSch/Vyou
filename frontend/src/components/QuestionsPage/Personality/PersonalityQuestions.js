db92debc789af873b120926bd794730e2159ef69
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-16 09:23:48 +0000

    feat: Added basic onClick fn to handle local state

--- frontend/src/components/QuestionsPage/Personality/PersonalityQuestions.js
+++ frontend/src/components/QuestionsPage/Personality/PersonalityQuestions.js
@@ -1,15 +1,58 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import QuestionFactory from '../Question/QuestionFactory';
+// import ReactPaginate from 'react-paginate';
 
 
 class PersonalityQuestions extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            categoryCount: 1
+        }
+        this.handlePageClick = this.handlePageClick.bind(this);
+    }
+
+    // componentDidUpdate(prevProps, prevState) {
+    //     // console.log(this.state.categoryCount);
+    //     if (this.state.categoryCount !== prevState.categoryCount) {
+    //         console.log(prevState.categoryCount);
+    //         this.handlePageClick();
+    //         // console.log('hi');
+    //     }
+    // }
+    handlePageClick(callback) {
+        console.log('fire', this.state.categoryCount);
+        this.setState (prevState => {
+            console.log(prevState.categoryCount);
+            return {
+                categoryCount: prevState.categoryCount + 1
+            }
+        }, () => {
+            console.log('fired', this.state.categoryCount);
+        });
+    }
+
     render() {
         const { questions } = this.props;
         return (
             <div>
                 <h2>Personality Questions</h2>
                 <QuestionFactory questionsArray={questions} index={1} />
+                <QuestionFactory questionsArray={questions} index={2} />
+                <QuestionFactory questionsArray={questions} index={3} />
+                <QuestionFactory questionsArray={questions} index={4} />
+                <button onClick={this.handlePageClick}>Next</button>
+                {/* <ReactPaginate 
+                    previousLabel={"previous"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    pageCount={8}
+                    onPageChange={this.handlePageClick(this.state.categoryCount)}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={8}
+                /> */}
             </div>
         );
     }
