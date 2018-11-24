53ef17f648c3473fa551955a43db922486924693
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-24 00:36:11 +0000

    feat: Added a setAnswer & resetAllAnswers actions

--- frontend/src/components/Answers/redux/actions.js
+++ frontend/src/components/Answers/redux/actions.js
@@ -1,14 +1,25 @@
 import actionTypes from './actionTypes';
 
-const setAnswer = (answer) => ({
+const setAnswer = (categoryId, answerValue, questionId, question, questionSign) => ({
   type: actionTypes.SET_ANSWER,
   payload: {
-    answer
+    categoryId: categoryId,
+    userAnswer: {
+      question: question,
+      questionId: questionId,
+      answerValue: answerValue,
+      questionSign: questionSign
+    }
   }
 });
 
+const resetAllAnswers = () => ({
+  type: actionTypes.RESET_ANSWERS
+});
+
 const answerActions = {
-  setAnswer
+  setAnswer,
+  resetAllAnswers
 };
 
 export default answerActions;
