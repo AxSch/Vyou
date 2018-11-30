e7045319af4275e9a1c7bd8ecfd586c4ea5f2185
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-30 14:26:30 +0000

    feat: Added functions to handle setAnswer & validateAnswers states

--- frontend/src/components/Answers/redux/reducer.js
+++ frontend/src/components/Answers/redux/reducer.js
@@ -2,35 +2,73 @@ import actionTypes from './actionTypes';
 import _ from 'lodash';
 
 const initialState = {
-  answered: {
-    1: []
+  answeredPersonality: {
+    1: {},
+    2: {},
+    3: {},
+    4: {},
+    5: {},
+    6: {},
+    7: {},
+    8: {},
+    9: {},
+    10: {},
+    11: {},
+    12: {},
+    13: {},
+    14: {},
+    15: {},
+    16: {}
   },
+  isValid: null
 }
 
-const selectorTest = (state, userAnswer, categoryId) => {
+const updateAnswer = (state, userAnswer, categoryId) => {
   let newState = _.clone(state);
-  if (newState.answered) {
-    newState.answered = {
-      ...newState.answered,
-      [categoryId]: [...newState.answered[categoryId], userAnswer]
-    };
-  }
+  newState.answeredPersonality = {
+    ...newState,
+    ...newState.answeredPersonality,
+    [categoryId]: {
+      ...newState.answeredPersonality[categoryId],
+      [userAnswer.questionId]: {
+        ...newState.answeredPersonality[categoryId][userAnswer.questionId],
+        question: userAnswer.question,
+        questionSign: userAnswer.questionSign,
+        value: userAnswer.answerValue,
+        lastUpdated: userAnswer.lastUpdated,
 
+      }
+    }
+  }
   return newState;
 }
+
+const validateAnswers = (state, answers, questions, categoryId) => {
+  let newState = _.clone(state);
+  if (newState.answeredPersonality[categoryId]) {
+    const answersArr = Object.values(newState.answeredPersonality[categoryId]);
+    if (answersArr.length === questions.length) {
+      newState.isValid = true;
+    } else {
+      newState.isValid = false;
+    }
+  }
+  return newState;
+}
+
 
 const answersReducer = (state=initialState, action) => {
   switch(action.type){
     case actionTypes.SET_ANSWER:
-      return selectorTest(state, action.payload.userAnswer, action.payload.categoryId);
+      const setAnswerState = updateAnswer(state, action.payload.userAnswer, action.payload.categoryId);
+      return setAnswerState;
     case actionTypes.SAVE_ANSWERS:
       return {
         ...state
       }
     case actionTypes.VALIDATE_ANSWERS:
-      return {
-        ...state
-      }
+      const validAnswerState = validateAnswers(state, action.payload.answers, action.payload.questions, action.payload.categoryId);
+      return validAnswerState;
     case actionTypes.RESET_ANSWERS:
       return initialState;
     default:
