f4f27af81dab90728fe6949ae02b7132511b9867
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-30 14:24:02 +0000

    feat: Added validateAnswers dispatcher  & personalityQuestions

--- frontend/src/components/Answers/Answers.js
+++ frontend/src/components/Answers/Answers.js
@@ -4,27 +4,46 @@ import * as moment from 'moment';
 
 
 class Answers extends Component {
+    constructor(props) {
+        super(props);
+        this.handleOnChange = this.handleOnChange.bind(this);
+    }
+
+    handleOnChange(e) {
+        const { personalityQuestions, answers, categoryId, questionId, setAnswer, questionText, questionSign, validateAnswers, questionType } = this.props;
+        switch(questionType) {
+            case 'PS':
+                const questionCategory = personalityQuestions.filter((question) => question.fields.QuestionCategory.id === categoryId);
+                setAnswer(categoryId, e.target.value, questionId, questionText, questionSign, moment().format());
+                validateAnswers(answers.answeredPersonality, questionCategory, categoryId);
+                break;
+            default:
+                return null;
+        }
+
+    }
+
     render() {
-        const { categoryId, questionId, setAnswer, questionText, questionSign } = this.props;
+        const { questionId } = this.props;
         return (
             <div>
                 <label htmlFor={`stronglyAgree-${questionId}`}>Strongly Agree</label>
-                <input type="radio" name={`answers-${questionId}`} value={3} id={`stronglyAgree-${questionId}`} onChange={(e) => setAnswer(categoryId, e.target.value, questionId, questionText, questionSign, moment().format())}/> 
+                <input type="radio" name={`answers-${questionId}`} value={3} id={`stronglyAgree-${questionId}`} onChange={(e) => this.handleOnChange(e)}/> 
                 
                 <label htmlFor="slightlyAgree">Slightly Agree</label>
-                <input type="radio" name={`answers-${questionId}`} value={2} id={`slightlyAgree-${questionId}`} onChange={(e) => setAnswer(categoryId, e.target.value, questionId, questionText, questionSign, moment().format())}/>  
+                <input type="radio" name={`answers-${questionId}`} value={2} id={`slightlyAgree-${questionId}`} onChange={(e) => this.handleOnChange(e)}/>  
                 
                 <label htmlFor={`agree-${questionId}`}>Agree</label>
-                <input type="radio" name={`answers-${questionId}`} value={1} id={`agree-${questionId}`} onChange={(e) => setAnswer(categoryId, e.target.value, questionId, questionText, questionSign, moment().format())} />  
+                <input type="radio" name={`answers-${questionId}`} value={1} id={`agree-${questionId}`} onChange={(e) => this.handleOnChange(e)} />  
                 
                 <label htmlFor={`disagree-${questionId}`}>Disagree</label>
-                <input type="radio" name={`answers-${questionId}`} value={-1} id={`disagree-${questionId}`} onChange={(e) => setAnswer(categoryId, e.target.value, questionId, questionText, questionSign, moment().format())} />  
+                <input type="radio" name={`answers-${questionId}`} value={-1} id={`disagree-${questionId}`} onChange={(e) => this.handleOnChange(e)} />  
                 
                 <label htmlFor={`slightlyDisagree-${questionId}`}>Slightly Disagree</label>
-                <input type="radio" name={`answers-${questionId}`} value={-2} id={`slightlyDisagree-${questionId}`} onChange={(e) => setAnswer(categoryId, e.target.value, questionId, questionText, questionSign, moment().format())} />  
+                <input type="radio" name={`answers-${questionId}`} value={-2} id={`slightlyDisagree-${questionId}`} onChange={(e) => this.handleOnChange(e)} />  
                 
                 <label htmlFor={`stronglyDisagree-${questionId}`}>Strongly Disagree</label>
-                <input type="radio" name={`answers-${questionId}`} value={-3} id={`stronglyDisagree-${questionId}`} onChange={(e) => setAnswer(categoryId, e.target.value, questionId, questionText, questionSign, moment().format())} />                         
+                <input type="radio" name={`answers-${questionId}`} value={-3} id={`stronglyDisagree-${questionId}`} onChange={(e) => this.handleOnChange(e)} />                         
             </div>
         );
     }
@@ -36,6 +55,7 @@ Answers.PropTypes = {
     categoryId: PropTypes.number,
     questionText: PropTypes.string,
     questionSign: PropTypes.string,
+    questionType: PropTypes.string,
 }
 
 export default Answers;
