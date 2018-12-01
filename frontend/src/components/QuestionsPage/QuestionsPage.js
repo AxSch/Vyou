5e45861b4a46a3f0d5bf0c0035166878c52daed7
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-01 18:18:23 +0000

    feat: Implemented switch to handle the rendering of questions based on category

--- frontend/src/components/QuestionsPage/QuestionsPage.js
+++ frontend/src/components/QuestionsPage/QuestionsPage.js
@@ -4,20 +4,63 @@ import PersonalityQuestions from './Personality/PersonalityQuestions';
 
 
 class QuestionsPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            category: '',
+        };
+
+        this.renderCategoryQuestions = this.renderCategoryQuestions.bind(this);
+        this.handleOnChange = this.handleOnChange.bind(this);
+    }
+
     componentDidMount() {
         const { fetchPersonalityQuestions } = this.props;
         fetchPersonalityQuestions();
     }
+    
+    handleOnChange(e) {
+        const value = e.target.value;
+        this.setState(prevState => {
+            return {
+                category: value
+            }
+        }, () => {
+            console.log(value)
+        });
+    }
+
+    renderCategoryQuestions (category) {
+        const { personalityQuestions, resetAllAnswers, answers } = this.props;
+        switch(category) {
+            case 'PERSONALITY':
+                return <PersonalityQuestions questions={personalityQuestions} answers={answers} resetAllAnswers={resetAllAnswers}/>;
+            default:
+                return null;
+        }
+    }
 
     render() {
-        const { personalityQuestions, resetAllAnswers, answers } = this.props;
+        const { category } = this.state;
         return (
             <div>
                 <h2>Questions</h2>
                 <div>
                     <h4>Progress</h4>
                 </div>
-                <PersonalityQuestions questions={personalityQuestions} answers={answers} resetAllAnswers={resetAllAnswers}/>
+                <div>
+                    <h4>Topics</h4>
+                    <label htmlFor={"personality"}>Personality</label>
+                    <input type="radio" name={"questions"} value={"PERSONALITY"} id={"personality"} onChange={(e) => this.handleOnChange(e)}/>
+                    <label htmlFor={"energyLevels"}>Energy Levels</label>
+                    <input type="radio" name={"questions"} value={"ENERGYLEVELS"} id={"energyLevels"} onChange={(e) => this.handleOnChange(e)}/>
+                    <label htmlFor={"energyFlow"}>Energy Flow</label>
+                    <input type="radio" name={"questions"} value={"ENERGYFLOW"} id={"energyFlow"} onChange={(e) => this.handleOnChange(e)}/>
+                    <label htmlFor={"energyMapping"}>Energy Mapping</label>
+                    <input type="radio" name={"questions"} value={"ENERGYMAPPER"} id={"energyMapping"} onChange={(e) => this.handleOnChange(e)}/>
+                </div>
+                {this.renderCategoryQuestions(category)}
             </div>
         );
     }
