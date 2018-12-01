d18178ecdd6082ed163318b9731b3e9c5d5ee573
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-01 18:38:42 +0000

    feat: Added a dispatcher for Energy Levels questions

--- frontend/src/components/QuestionsPage/redux/dispatchers.js
+++ frontend/src/components/QuestionsPage/redux/dispatchers.js
@@ -16,4 +16,24 @@ const requestPSQuestionsDispatcher = () => {
   }
 }
 
-export default requestPSQuestionsDispatcher;
+const requestELQuestionsDispatcher = () => {
+  return dispatch => {
+    dispatch(questionRequestActions.questionRequest);
+    questionsService.energyLevelQuestions()
+      .then(
+        response => {
+          dispatch(questionRequestActions.questionRequestSuccess(response.data));
+        },
+        error => {
+          dispatch(questionRequestActions.questionRequestFailure(error.data));
+        }
+      );
+  }
+}
+
+const questionsDispatchers = {
+  requestPSQuestionsDispatcher,
+  requestELQuestionsDispatcher
+}
+
+export default questionsDispatchers;
