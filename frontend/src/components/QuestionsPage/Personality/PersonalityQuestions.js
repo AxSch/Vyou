df6c7dbfb736e2e99dbd125dbaef7465c367f24a
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-05 15:31:19 +0000

    chore: Cleaned up code

--- frontend/src/components/QuestionsPage/Personality/PersonalityQuestions.js
+++ frontend/src/components/QuestionsPage/Personality/PersonalityQuestions.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import QuestionFactory from '../Question/QuestionFactory';
-// import ReactPaginate from 'react-paginate';
 
 
 class PersonalityQuestions extends Component {
