cb3c2799d0af30cb4f04fcf63ca96a0f5e7086bd
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-06 12:31:44 +0000

    feat: Exposed userLoggedIn to component

--- frontend/src/components/AccountPage/Profile/ProfileContainer.js
+++ frontend/src/components/AccountPage/Profile/ProfileContainer.js
@@ -3,6 +3,7 @@ import Profile from './Profile';
 
 const mapStateToProps = (state, ownProps) => ({
     userEmail: state.login.userEmail,
+    userLoggedIn: state.login.loggedIn,
 });
 
 const ProfileContainer = connect(mapStateToProps)(Profile);
