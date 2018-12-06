3b7fa89216bcf0849dc9eb1459e4200a6cdaf0ae
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-06 14:55:58 +0000

    feat: Implemented newly created reducers - profile & users to the main reducer

--- frontend/src/mainRedux/mainReducer.js
+++ frontend/src/mainRedux/mainReducer.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import { loginReducer } from '../components/Login/redux/reducer';
 import questionsReducer from '../components/QuestionsPage/redux/reducer';
 import answersReducer from '../components/Answers/redux/reducer';
-// import { questionsReducer } from '../components/QuestionsPage/redux/reducer';
+import { profileReducer, usersReducer } from '../components/AccountPage/redux/reducer';
 
 const appReducer = combineReducers({
   login: loginReducer,
   questions: questionsReducer,
-  answers: answersReducer
+  answers: answersReducer,
+  users: usersReducer,
+  profile: profileReducer
 })
 
 export default appReducer;
