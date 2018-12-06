79f0ebd39f68fa9a34193c8100612e02d1e3b5f6
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-06 14:53:30 +0000

    feat: Added getUsers endpoint

--- frontend/src/services/profileService.js
+++ frontend/src/services/profileService.js
@@ -1,11 +1,17 @@
 import request from "axios";
 
+const getUsers = () => {
+  return request
+  .get("/api/v1/users/")
+}
+
 const getUserProfile = () => {
   return request
   .get("/api/v1/profiles/")
 }
 
 const profileService = {
+  getUsers,
   getUserProfile
 }
 export default profileService;
