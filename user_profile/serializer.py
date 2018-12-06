f91c2c98986790e918b997c9d643bf3f542c5d55
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-06 14:52:27 +0000

    feat: Added id to serializers

--- user_profile/serializer.py
+++ user_profile/serializer.py
@@ -4,6 +4,6 @@ from . import models
 class ProfileSerializer(serializers.ModelSerializer):
   class Meta:
    model = models.Profile
-   fields = ('name', 'gender', 'dob', 'address', 'job_title', 'job_industry', 
+   fields = ('id', 'name', 'sex', 'dob', 'address', 'job_title', 'job_industry', 
    'bio', 'education', 'user_img', 'creation_date', 'last_update', 'facebook_profile', 'linkedIn_profile',
    'twitter_profile', 'gitHub_profile', 'instagram_profile')
