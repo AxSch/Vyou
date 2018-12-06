a0a3bb8b73efd0d3063aa0f3019b66acd1b39d6b
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-06 17:39:18 +0000

    feat: Exposed createProfile dispatcher to component

--- frontend/src/components/AccountPage/AccountSettings/AccountSettingsContainer.js
+++ frontend/src/components/AccountPage/AccountSettings/AccountSettingsContainer.js
@@ -2,11 +2,18 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
 import AccountSettings from './AccountSettings';
+import profileDispatchers from '../redux/dispatchers';
 
 const mapStateToProps = (state, ownProps) => ({
     userEmail: state.login.userEmail
 });
 
-const AccountSettingsContainer = withRouter(connect(mapStateToProps)(AccountSettings));
+const mapDispatchToProps = dispatch => (
+    bindActionCreators({
+        createProfile: profileDispatchers.getProfileDispatcher
+    }, dispatch)
+);
+
+const AccountSettingsContainer = withRouter(connect(mapStateToProps, mapDispatchToProps)(AccountSettings));
 
 export default AccountSettingsContainer;
