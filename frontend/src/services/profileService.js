d754eca15f4e719e95ecc3a1bbcc29d962737469
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-06 17:09:44 +0000

    feat: Added user profile creation endpoint

--- frontend/src/services/profileService.js
+++ frontend/src/services/profileService.js
@@ -10,8 +10,30 @@ const getUserProfile = (userId) => {
   .get(`/api/v1/profiles/${userId}`)
 }
 
+const createUserProfile = (userId, userProfileData) => {
+  return request
+  .post(`/api/v1/profiles/${userId}`, {
+    user: userProfileData.id,
+    name: userProfileData.name,
+    sex: userProfileData.sex,
+    dob: userProfileData.dob,
+    address: userProfileData.address,
+    job_title: userProfileData.job_title,
+    job_industry: userProfileData.job_industry,
+    bio: userProfileData.bio,
+    education: userProfileData.education,
+    user_img: userProfileData.user_img,
+    facebook_profile: userProfileData.facebook_profile,
+    linkedIn_profile: userProfileData.linkedIn_profile,
+    twitter_profile: userProfileData.twitter_profile,
+    gitHub_profile: userProfileData.gitHub_profile,
+    instagram_profile: userProfileData.instagram_profile,
+  })
+}
+
 const profileService = {
   getUsers,
-  getUserProfile
+  getUserProfile,
+  createUserProfile
 }
 export default profileService;
