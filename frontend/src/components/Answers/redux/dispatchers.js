d2317029b85388b50646b0590dd90b15e35da9e6
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-06 01:23:23 +0000

    feat: Added subCategoryId param to setAnswer & validateAnswer dispatchers to reflect action change

--- frontend/src/components/Answers/redux/dispatchers.js
+++ frontend/src/components/Answers/redux/dispatchers.js
@@ -1,15 +1,15 @@
 import answerActions from './actions';
 
 
-const setAnswerDispatcher = (categoryId, answerValue, questionId, question, questionSign, lastUpdated, questionType) => {
+const setAnswerDispatcher = (categoryId, answerValue, questionId, question, questionSign, lastUpdated, questionType, subCategoryId) => {
   return dispatch => {
-    dispatch(answerActions.setAnswer(categoryId, Number(answerValue), questionId, question, questionSign, lastUpdated, questionType));
+    dispatch(answerActions.setAnswer(categoryId, Number(answerValue), questionId, question, questionSign, lastUpdated, questionType, subCategoryId));
   }
 }
 
-const validateAnswersDispatcher = (answers, questions, categoryId, questionType) => {
+const validateAnswersDispatcher = (answers, questions, categoryId, questionType, subCategoryId) => {
   return dispatch => {
-    dispatch(answerActions.validateAnswers(answers, questions, categoryId, questionType))
+    dispatch(answerActions.validateAnswers(answers, questions, categoryId, questionType, subCategoryId))
   }
 }
 
