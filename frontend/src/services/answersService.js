f176c251cec608c621ccec446a938463a1a5b382
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-09 15:52:39 +0000

    refact: Changed data being sent to the endpoint

--- frontend/src/services/answersService.js
+++ frontend/src/services/answersService.js
@@ -3,13 +3,14 @@ import request from "axios";
 const sendPersonalityAnswers = (userId, question) => {
   return request
   .post("/api/v1/answers/personality_answers/", {
-  questionId: question,
-  question: question,
-  category_name: question,
-  scale_alpha: question,
-  answer_text: question,
-  answer_score: question,
-  category_id: question,
+  question_id: question.questionId,
+  question: question.question,
+  question_sign: question.questionSign,
+  category_name: question.category_name,
+  scale_alpha: question.scale_alpha,
+  answer_text: question.answer_text,
+  answer_score: question.answer_score,
+  category_id: question.category_id,
   profile: userId
   }
 )
