1bd3384d3b68d19b22dceb6177056fbd1f398e60
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-10 20:51:57 +0000

    refact&feat: Added fetchCompEFAnswers() & changed params

--- frontend/src/services/answersService.js
+++ frontend/src/services/answersService.js
@@ -1,15 +1,15 @@
 import request from "axios";
 
-const sendPersonalityAnswers = (userId, question, index) => {
+const sendPersonalityAnswers = (userId, answer, index) => {
   return request
   .post("/api/v1/answers/personality_answers/", {
   question_id: index,
-  question: question.question,
-  question_sign: question.questionSign,
-  category_name: question.categoryName,
-  scale_alpha: question.scaleAlpha,
-  answer_score: question.value,
-  category_id: question.categoryId,
+  question: answer.question,
+  question_sign: answer.questionSign,
+  category_name: answer.categoryName,
+  scale_alpha: answer.scaleAlpha,
+  answer_score: answer.value,
+  category_id: answer.categoryId,
   profile: userId
   }
 )
@@ -30,30 +30,26 @@ const updatePersonalityAnswers = (userId, answer, index) => {
 )
 }
 
-const sendEnergyFlowAnswers = (userId, question, index) => {
+const sendEnergyFlowAnswers = (userId, answer, index) => {
   return request
-  .post("/api/v1/answers/personality_answers/", {
-  question_id: index,
-  question: question.question,
-  question_sign: question.questionSign,
-  category_name: question.categoryName,
-  scale_alpha: question.scaleAlpha,
-  answer_score: question.value,
-  category_id: question.categoryId,
-  profile: userId
+  .post("/api/v1/answers/energy_flow_answers/", {
+    question_id: answer.question_id,
+    question: answer.question,
+    category_name: answer.category_name,
+    subcategory_name: answer.category_id,
+    answer_score: answer.answer_score,
+    profile: userId
   }
 )
 }
 
 const updateEnergyFlowAnswers = (userId, answer, index) => {
   return request
-  .put(`/api/v1/answers/personality_answers/${index}/`, {
+  .put(`/api/v1/answers/energy_flow_answers/${index}/`, {
   question_id: answer.question_id,
   question: answer.question,
-  question_sign: answer.question_sign,
   category_name: answer.category_name,
-  category_id: answer.category_id,
-  scale_alpha: answer.scale_alpha,
+  subcategory_name: answer.category_id,
   answer_score: answer.answer_score,
   profile: userId
   }
@@ -71,6 +67,17 @@ const fetchCompPersonalityAnswers = (userId, categoryId) => {
 )
 }
 
+const fetchCompEFAnswers = (userId, categoryId) => {
+  return request
+  .get("/api/v1/answers/energy_flow_answers/", {
+    params: {
+      profile: userId,
+      category_id: categoryId
+    }
+  }
+)
+}
+
 
 
 const answersService = {
@@ -78,7 +85,8 @@ const answersService = {
   updatePersonalityAnswers,
   sendEnergyFlowAnswers,
   updateEnergyFlowAnswers,
-  fetchCompPersonalityAnswers
+  fetchCompPersonalityAnswers,
+  fetchCompEFAnswers
 };
 
 export default answersService;
