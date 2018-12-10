b478be73bc41de6ca93988c849a22819929a57ad
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-10 20:42:57 +0000

    feat: Added EnergyFlowAnswers endpoints for send & update

--- frontend/src/services/answersService.js
+++ frontend/src/services/answersService.js
@@ -30,6 +30,36 @@ const updatePersonalityAnswers = (userId, answer, index) => {
 )
 }
 
+const sendEnergyFlowAnswers = (userId, question, index) => {
+  return request
+  .post("/api/v1/answers/personality_answers/", {
+  question_id: index,
+  question: question.question,
+  question_sign: question.questionSign,
+  category_name: question.categoryName,
+  scale_alpha: question.scaleAlpha,
+  answer_score: question.value,
+  category_id: question.categoryId,
+  profile: userId
+  }
+)
+}
+
+const updateEnergyFlowAnswers = (userId, answer, index) => {
+  return request
+  .put(`/api/v1/answers/personality_answers/${index}/`, {
+  question_id: answer.question_id,
+  question: answer.question,
+  question_sign: answer.question_sign,
+  category_name: answer.category_name,
+  category_id: answer.category_id,
+  scale_alpha: answer.scale_alpha,
+  answer_score: answer.answer_score,
+  profile: userId
+  }
+)
+}
+
 const fetchCompPersonalityAnswers = (userId, categoryId) => {
   return request
   .get("/api/v1/answers/personality_answers/", {
@@ -46,6 +76,8 @@ const fetchCompPersonalityAnswers = (userId, categoryId) => {
 const answersService = {
   sendPersonalityAnswers,
   updatePersonalityAnswers,
+  sendEnergyFlowAnswers,
+  updateEnergyFlowAnswers,
   fetchCompPersonalityAnswers
 };
 
