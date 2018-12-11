2282fc0833bcf99f87779f2637c0e1c3b3542e22
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-11 03:18:35 +0000

    feat: Added endpoints for update, send & fetch EnergyMappingAnswers

--- frontend/src/services/answersService.js
+++ frontend/src/services/answersService.js
@@ -42,6 +42,21 @@ const sendEnergyLevelAnswers = (userId, answer, index) => {
 )
 }
 
+const sendEnergyMappingAnswers = (userId, answer, index) => {
+  return request
+  .post("/api/v1/answers/energy_mapping_answers/", {
+    question_id: index,
+    question: answer.question,
+    category_name: answer.categoryName,
+    category_id: answer.categoryId,
+    subcategory_name: answer.subCategoryName,
+    subcategory_id: answer.subCategoryId,
+    answer_score: answer.value,
+    profile: userId
+  }
+)
+}
+
 const updatePersonalityAnswers = (userId, answer, index) => {
   return request
   .put(`/api/v1/answers/personality_answers/${index}/`, {
@@ -84,6 +99,21 @@ const updateEnergyLevelAnswers = (userId, answer, index) => {
 )
 }
 
+const updateEnergyMappingAnswers = (userId, answer, index) => {
+  return request
+  .put(`/api/v1/answers/energy_mapping_answers/${index}/`, {
+    question_id: answer.question_id,
+    question: answer.question,
+    category_name: answer.category_name,
+    category_id: answer.category_id,
+    subcategory_name: answer.subcategory_name,
+    subcategory_id: answer.subcategory_id,
+    answer_score: answer.answer_score,
+    profile: userId
+  }
+)
+}
+
 const fetchCompPersonalityAnswers = (userId, categoryId) => {
   return request
   .get("/api/v1/answers/personality_answers/", {
@@ -117,18 +147,33 @@ const fetchCompELAnswers = (userId, categoryId) => {
 )
 }
 
+const fetchCompEMAnswers = (userId, categoryId) => {
+  return request
+  .get("/api/v1/answers/energy_mapping_answers/", {
+    params: {
+      profile: userId,
+      category_id: categoryId
+    }
+  }
+)
+}
+
 
 
 const answersService = {
   sendPersonalityAnswers,
   sendEnergyFlowAnswers,
   sendEnergyLevelAnswers,
+  sendEnergyMappingAnswers,
   updatePersonalityAnswers,
   updateEnergyFlowAnswers,
   updateEnergyLevelAnswers,
+  updateEnergyMappingAnswers,
   fetchCompPersonalityAnswers,
   fetchCompEFAnswers,
-  fetchCompELAnswers
+  fetchCompELAnswers,
+  fetchCompEMAnswers,
+
 };
 
 export default answersService;
