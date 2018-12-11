472ecd9322741660411168bbdfd33b6d26297acc
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-11 21:20:47 +0000

    fix: Resolved import

--- frontend/src/mainRedux/mainReducer.js
+++ frontend/src/mainRedux/mainReducer.js
@@ -3,7 +3,7 @@ import { loginReducer } from '../components/Login/redux/reducer';
 import questionsReducer from '../components/QuestionsPage/redux/reducer';
 import answersReducer from '../components/Answers/redux/reducer';
 import { profileReducer, usersReducer } from '../components/AccountPage/redux/reducer';
-import { registrationReducer } from  '../components/Registration/redux/reducer';
+import registrationReducer from  '../components/Registration/redux/reducer';
 
 
 const appReducer = combineReducers({
