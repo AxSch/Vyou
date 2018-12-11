ba95a480ed0861544368d2dad2f2b06049af311b
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-11 20:54:34 +0000

    feat: Added basic actions

--- frontend/src/components/QuestionsPage/redux/actions.js
+++ frontend/src/components/QuestionsPage/redux/actions.js
@@ -1,24 +1,4 @@
 import actionTypes from './actionTypes';
-import { Action } from 'redux';
-
-// interface QuestionAction extends Action {
-//   payload: {
-//     questions?: Array<Questions>,
-//     error?: string | null,
-    
-//   }
-// }
-
-// interface QuestionFields {
-//   question: String,
-//   subcategory: String,
-//   category: String,
-// }
-
-// interface Questions {
-//   id: Number,
-//   fields: QuestionFields,
-// }
 
 
 const questionRequest = () => ({
