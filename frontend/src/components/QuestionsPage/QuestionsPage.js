540f23ffbab7434e451d02f23e1ec2f2657ce8b2
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-13 19:54:08 +0000

    feat: Added conditional rendering based on userLogIn

--- frontend/src/components/QuestionsPage/QuestionsPage.js
+++ frontend/src/components/QuestionsPage/QuestionsPage.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import PersonalityQuestions from './Personality/PersonalityQuestions';
 import EnergyFlowQuestions from './EnergyFlow/EnergyFlowQuestions';
 import EnergyLevelQuestions from './EnergyLevel/EnergyLevelQuestions';
 import EnergyMappingQuestions from './EnergyMapping/EnergyMappingQuestions';
+import RequestUserLogIn from '../Login/RequestUserLogIn/RequestUserLogIn';
 
 
 class QuestionsPage extends Component {
@@ -74,26 +75,40 @@ class QuestionsPage extends Component {
         }
     }
 
-    render() {
+    checkUserLoggedIn(isLogged){
         const { category } = this.state;
+        
+        if (isLogged) {
+            return (
+                <Fragment>
+                    <h2>Questions</h2>
+                    <div>
+                        <h4>Progress</h4>
+                    </div>
+                    <div>
+                        <h4>Topics</h4>
+                        <label htmlFor={"personality"}>Personality</label>
+                        <input type="radio" name={"questions"} value={"PERSONALITY"} id={"personality"} onChange={(e) => this.handleOnChange(e)}/>
+                        <label htmlFor={"energyLevel"}>Energy Levels</label>
+                        <input type="radio" name={"questions"} value={"ENERGYLEVEL"} id={"energyLevel"} onChange={(e) => this.handleOnChange(e)}/>
+                        <label htmlFor={"energyFlow"}>Energy Flow</label>
+                        <input type="radio" name={"questions"} value={"ENERGYFLOW"} id={"energyFlow"} onChange={(e) => this.handleOnChange(e)}/>
+                        <label htmlFor={"energyMapping"}>Energy Mapping</label>
+                        <input type="radio" name={"questions"} value={"ENERGYMAPPING"} id={"energyMapping"} onChange={(e) => this.handleOnChange(e)}/>
+                    </div>
+                    {this.renderCategoryQuestions(category)}
+                </Fragment>
+            );
+        } else {
+            return <RequestUserLogIn />
+        }
+    }
+
+    render() {
+        const { login } = this.props;
         return (
             <div>
-                <h2>Questions</h2>
-                <div>
-                    <h4>Progress</h4>
-                </div>
-                <div>
-                    <h4>Topics</h4>
-                    <label htmlFor={"personality"}>Personality</label>
-                    <input type="radio" name={"questions"} value={"PERSONALITY"} id={"personality"} onChange={(e) => this.handleOnChange(e)}/>
-                    <label htmlFor={"energyLevel"}>Energy Levels</label>
-                    <input type="radio" name={"questions"} value={"ENERGYLEVEL"} id={"energyLevel"} onChange={(e) => this.handleOnChange(e)}/>
-                    <label htmlFor={"energyFlow"}>Energy Flow</label>
-                    <input type="radio" name={"questions"} value={"ENERGYFLOW"} id={"energyFlow"} onChange={(e) => this.handleOnChange(e)}/>
-                    <label htmlFor={"energyMapping"}>Energy Mapping</label>
-                    <input type="radio" name={"questions"} value={"ENERGYMAPPING"} id={"energyMapping"} onChange={(e) => this.handleOnChange(e)}/>
-                </div>
-                {this.renderCategoryQuestions(category)}
+                {this.checkUserLoggedIn(login.loggedIn)}
             </div>
         );
     }
