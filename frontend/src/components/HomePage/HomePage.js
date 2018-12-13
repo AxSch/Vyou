c19005d58c478456d15198fe01827b6dd76cc44a
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-13 20:06:23 +0000

    feat: Add renderLoggoutMsg() & internal state for handling the logout message

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,26 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 class HomePage extends Component {
-  // renderLogin = () => {
-  //   return (
-      
-  //   );
-  // }
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        isLoggedIn: true,
+    };
+  }
+
+  handleLogout(e) {
     const { logout } = this.props;
-    console.log(typeof logout);
-    // console.log(typeof logoutDispatcher);
+    this.setState(() => {
+      return {
+        isLoggedIn: false
+      }
+    }, () => {});
+    logout();
+  }
+
+  renderLoggoutMsg(logoutMsg, isLoggedIn) {
+    if (isLoggedIn === false && logoutMsg !== undefined) {
+      return <p>{logoutMsg.detail}</p>
+    }
+    return null;
+  }
+  
+  render() {
+    const { logoutMsg, userLoggedIn } = this.props;
+    const { isLoggedIn } = this.state;
+
     return (
       <div>
         <div>
           <h1>NavBar</h1>
           <ul>
-            <li><Link to="/accounts/login">Login</Link></li>
+            {!userLoggedIn ? <li><Link to="/accounts/login">Login</Link></li> : null}
             <li><Link to="/accounts/profile">Profile</Link></li>
             <li><Link to="/questions">Questions</Link></li>
-            <li><Link to="/register">Register</Link></li>
-            <li><button onClick={() => logout()} to="/">Logout</button></li>
+            {!userLoggedIn ? <li><Link to="/register">Register</Link></li> : null}
+            {userLoggedIn ? <li><button onClick={(e) => this.handleLogout(e)} to="/">Logout</button></li> : null}
+            {this.renderLoggoutMsg(logoutMsg, isLoggedIn)}
           </ul>
         </div>
       </div>
