700ae8c438ca31558e71d36294722af43e4e4a93
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-18 03:40:35 +0000

    refact: Updated dispatchers to reflect the new implementation of the success action

--- frontend/src/components/Scores/redux/dispatchers.js
+++ frontend/src/components/Scores/redux/dispatchers.js
@@ -1,13 +1,13 @@
 import answerActions from './actions';
 import calculatedScoreService from '../../../services/calculatedScoreService';
 
-const getPSScoreDispatcher = (profile) => {
+const getPSScoreDispatcher = (profile, subject) => {
   return dispatch => {
     dispatch(answerActions.fetchCalculatedScore());
     calculatedScoreService.personalityScore(profile)
     .then(
       response => {
-        dispatch(answerActions.fetchCalculatedSuccess(response.data));
+        dispatch(answerActions.fetchCalculatedSuccess(response.data, subject));
       },
       error => {
         dispatch(answerActions.fetchCalculatedAnswersFailure(error.data));
@@ -16,13 +16,13 @@ const getPSScoreDispatcher = (profile) => {
   }
 }
 
-const getENScoreDispatcher = (profile)  => {
+const getENScoreDispatcher = (profile, subject)  => {
     return dispatch => {
       dispatch(answerActions.fetchCalculatedScore());
       calculatedScoreService.energyScore(profile)
       .then(
         response => {
-          dispatch(answerActions.fetchCalculatedSuccess(response.data));
+          dispatch(answerActions.fetchCalculatedSuccess(response.data, subject));
         },
         error => {
           dispatch(answerActions.fetchCalculatedAnswersFailure(error.data));
