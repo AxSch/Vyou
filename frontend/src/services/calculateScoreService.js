2e3e75e43239b7cecd6257ef6b59f85f98a64bed
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-18 03:16:26 +0000

    refact&feat: Implemented new endpoints & changed service name

---
+++ frontend/src/services/calculateScoreService.js
@@ -0,0 +1,18 @@
+import request from "axios";
+
+const personalityScore = (profile) => {
+  return request
+  .get(`/api/v1/calculations/personality_score/${profile}/`)
+}
+
+const energyScore = (profile) => {
+  return request
+  .get(`/api/v1/calculations/energy_score/${profile}/`)
+}
+
+const calculateScoreService = {
+  personalityScore,
+  energyScore
+}
+
+export default calculateScoreService;
