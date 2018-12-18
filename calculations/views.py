5f7f24f572c3d7c3d3a654f9e29b12b6e4083329
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-18 22:27:42 +0000

    feat: Added checking for querysets & checkModelExists()

--- calculations/views.py
+++ calculations/views.py
@@ -9,6 +9,14 @@ from question_ans.models import (
   EnergyMappingAnswers
 )
 # Create your views here.
+def checkModelExists(queryset):
+  if queryset.exists():
+    result = [answer.answer_score for answer in queryset]
+    return result
+  else:
+    result = 0
+    return result
+
 
 class EnergyAnswersScoreView(APIView):
   def get(self, request, format=None, *args,**kwargs):
@@ -16,11 +24,10 @@ class EnergyAnswersScoreView(APIView):
     queryset = EnergyLevelAnswers.objects.all()
     queryset2 = EnergyFlowAnswers.objects.all()
     queryset3 = EnergyMappingAnswers.objects.all()
-
-    energy_level_scores = [answer.answer_score for answer in queryset]
-    energy_flow_scores = [answer.answer_score for answer in queryset2]
-    energy_mapping_scores = [answer.answer_score for answer in queryset3]
-
+    
+    energy_level_scores = checkModelExists(queryset)
+    energy_flow_scores = checkModelExists(queryset2)
+    energy_mapping_scores = checkModelExists(queryset3)
     result = int(np.sum(energy_level_scores)) + int(np.sum(energy_flow_scores)) + int(np.sum(energy_mapping_scores))
     
     return JsonResponse({'result': result})
@@ -31,10 +38,19 @@ class PersonalityAnswersScoreView(APIView):
     queryset = PersonalityAnswers.objects.filter(profile=profile, question_sign="negative")
     queryset2 = PersonalityAnswers.objects.filter(profile=profile, question_sign="positive")
     
-    neg_scores = [answer.answer_score * -1 for answer in queryset]
-    total_neg_score = int(np.sum(neg_scores))
-    pos_scores = [answer.answer_score for answer in queryset2]
-    total_pos_score = int(np.sum(pos_scores))
+    neg_scores = []
+    pos_scores = []
+    if queryset.exists():
+      neg_scores = [answer.answer_score * -1 for answer in queryset]
+      total_neg_score = int(np.sum(neg_scores))
+    else:
+      total_neg_score = 0
+
+    if queryset2.exists():
+      pos_scores = [answer.answer_score for answer in queryset]
+      total_pos_score = int(np.sum(pos_scores))
+    else:
+      total_pos_score = 0
 
     result = total_pos_score + total_neg_score
     return JsonResponse({'result': result})
