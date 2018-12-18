fc6f9df633b4d658adf4e0589b6fb01c87f9c12c
K.A. Schuneman <alex_schuneman@outlook.com>
2018-12-18 03:56:23 +0000

    feat: Added scoresReducer to mainReducer

--- frontend/src/mainRedux/mainReducer.js
+++ frontend/src/mainRedux/mainReducer.js
@@ -4,6 +4,7 @@ import questionsReducer from '../components/QuestionsPage/redux/reducer';
 import answersReducer from '../components/Answers/redux/reducer';
 import { profileReducer, usersReducer } from '../components/AccountPage/redux/reducer';
 import registrationReducer from  '../components/Registration/redux/reducer';
+import scoresReducer from  '../components/Scores/redux/reducer';
 
 
 const appReducer = combineReducers({
@@ -12,7 +13,8 @@ const appReducer = combineReducers({
   answers: answersReducer,
   users: usersReducer,
   profile: profileReducer,
-  registration: registrationReducer
+  registration: registrationReducer,
+  userScores: scoresReducer
 })
 
 export default appReducer;
