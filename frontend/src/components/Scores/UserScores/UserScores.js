c577d81e8e1cda0228b6eb7baabd582884f1a37d
Paul Hendrick <paul.hendrick@gmail.com>
2019-02-11 16:17:14 +0000

    removed old calculations code from UserScores

--- frontend/src/components/Scores/UserScores/UserScores.js
+++ frontend/src/components/Scores/UserScores/UserScores.js
@@ -20,23 +20,6 @@ class UserScores extends Component {
   render() {
     const { energyScore, personalityScore  } = this.props;
 
-    let groupedPersonalityScores = _.groupBy(personalityScore.scores, 'category');
-    groupedPersonalityScores = Object.keys(groupedPersonalityScores).map((key) => {
-      return {label: key, scores: groupedPersonalityScores[key]}
-    });
-
-    const personalityChart = <div className="score-chart-container">
-        {groupedPersonalityScores.map((score, i) => {
-          let total = _.sumBy(score.scores, 'score');
-          let height = `${total*10}px`;
-          let score_key = `${i}`;
-
-          return <div key={score_key} className='score' style={{height: height}}>
-            <h3>{score.label}</h3>
-          </div>
-        })}
-    </div>
-
     return (
       <React.Fragment>
         <div>
